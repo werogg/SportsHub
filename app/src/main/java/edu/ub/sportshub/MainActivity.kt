8d0067c4b6c6daef93f375330264212a58c9af84
werogg <supwer00@gmail.com>
2020-03-04 00:48:16 +0100

    Initial commit

---
+++ app/src/main/java/edu/ub/sportshub/MainActivity.kt
@@ -0,0 +1,15 @@
+package edu.ub.sportshub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
