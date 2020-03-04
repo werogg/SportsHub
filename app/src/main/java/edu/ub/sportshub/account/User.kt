dbb3ecfa821ad3a76cd367416bc93eaf99db8c1c
werogg <supwer00@gmail.com>
2020-03-04 18:23:24 +0100

    Init user-model

---
+++ app/src/main/java/edu/ub/sportshub/account/User.kt
@@ -0,0 +1,49 @@
+package edu.ub.sportshub.account
+
+class User(
+    username: String,
+    fullname: String,
+    biography: String,
+    mail: String,
+    profilePicture: String,
+    uid: String,
+    banned: Boolean
+) {
+
+    private var username : String = username
+        get() = field
+        set(value) {
+            field = value
+        }
+    private var fullname : String = fullname
+        get() = field
+        set(value) {
+            field = value
+        }
+    private var biography : String = biography
+        get() = field
+        set(value) {
+            field = value
+        }
+    private var mail : String = mail
+        get() = field
+        set(value) {
+            field = value
+        }
+    private var profilePicture : String = profilePicture
+        get() = field
+        set(value) {
+            field = value
+        }
+    private var uid : String = uid
+        get() = field
+        set(value) {
+            field = value
+        }
+    private var banned : Boolean = banned
+        get() = field
+        set(value) {
+            field = value
+        }
+
+}
