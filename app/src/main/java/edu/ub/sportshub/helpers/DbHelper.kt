eeae873a6bf33b6f483f8f2ecc308f5707587b8d
Joel Otero <supwer00@gmail.com>
2020-03-10 17:39:28 +0100

    DbHelper init (#6)

--- app/src/main/java/edu/ub/sportshub/helpers/DbHelper.kt
+++ app/src/main/java/edu/ub/sportshub/helpers/DbHelper.kt
@@ -1,5 +1,63 @@
 package main.java.edu.ub.sportshub.helpers
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import edu.ub.sportshub.models.User
+
 class DbHelper {
+
+    private var mFirebaseAuth = FirebaseAuth.getInstance()
+    private var mFirebaseStore = FirebaseFirestore.getInstance()
+
+    fun getLoggedUser() : User? {
+        if (mFirebaseAuth.currentUser != null) {
+            var uid = mFirebaseAuth.uid
+            var userCollection = mFirebaseStore.collection(uid.toString())
+            var mail = userCollection.document("email") as String
+            var username = userCollection.document("username") as String
+            var fullname = userCollection.document("fullname") as String
+            var profilePicture = userCollection.document("profilePicture") as String
+
+            User(
+                mail,
+                username,
+                fullname,
+                profilePicture,
+                "",
+                "",
+                true
+            )
+        }
+        return null
+    }
+
+
+    fun createAccount(email: String,
+                      password: String,
+                      username : String,
+                      fullname : String,
+                      profilePicture : String
+                      ) : Boolean {
+        var success = false
+
+        mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+
+
+
+                mFirebaseAuth.currentUser.uid
+
+
+
+                success = it.isSuccessful
+            }
+
+        return success
+    }
+
+
+
+
+
 
 }
