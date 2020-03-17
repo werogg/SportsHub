5d9a1d5753f06d90dcae509f750e704fa48acb04
werogg <supwer00@gmail.com>
2020-03-17 13:28:09 +0100

    Front-end init

---
+++ app/src/main/java/edu/ub/sportshub/event/EventActivity.kt
@@ -0,0 +1,13 @@
+package edu.ub.sportshub.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.ub.sportshub.R
+
+class EventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event)
+    }
+}
---
+++ app/src/main/java/edu/ub/sportshub/profile/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package edu.ub.sportshub.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.ub.sportshub.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
