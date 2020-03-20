203584c3352dd862b825e725ee60acd4d8f0e2a1
Samuel <masael2000@gmail.com>
2020-03-20 18:10:19 +0100

    2.1 Profile Screen Design & 2.1.1 Settings Profile Screen

--- app/src/main/java/edu/ub/sportshub/home/HomeActivity.kt
+++ app/src/main/java/edu/ub/sportshub/home/HomeActivity.kt
@@ -1,16 +1,26 @@
 package edu.ub.sportshub.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import edu.ub.sportshub.R
 import edu.ub.sportshub.helpers.AuthDatabaseHelper
+import edu.ub.sportshub.profile.ProfileActivity
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val textprofile = findViewById<TextView>(R.id.txt_name)
+        //entrar al profile.
+        textprofile.setOnClickListener(){
+            val popupIntent = Intent(this, ProfileActivity::class.java)
+            startActivity(popupIntent)
+        }
     }
 
     fun logout(view : View) {
--- app/src/main/java/edu/ub/sportshub/profile/EditProfileActivity.kt
+++ app/src/main/java/edu/ub/sportshub/profile/EditProfileActivity.kt
@@ -5,27 +5,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import edu.ub.sportshub.R
 import edu.ub.sportshub.home.HomeActivity
 
-class ProfileActivity : AppCompatActivity() {
+
+class EditProfileActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-        val txteditprofile = findViewById<TextView>(R.id.txt_edit)
-
-        txteditprofile.setOnClickListener(){
-            val popupIntent = Intent(this, EditProfileActivity::class.java)
-            startActivity(popupIntent)
-        }
+        setContentView(R.layout.activity_editprofile)
 
         val home = findViewById<TextView>(R.id.size)
         home.setOnClickListener(){
             val gohome = Intent(this,HomeActivity::class.java)
             startActivity(gohome)
+        }
+
+        val validate = findViewById<ImageButton>(R.id.btn_validate)
+        validate.setOnClickListener(){
+            val goprofile = Intent(this,ProfileActivity::class.java)
+            startActivity(goprofile)
         }
     }
 }
