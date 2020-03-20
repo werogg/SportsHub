a2e75614eab4c97fa32dc79db2926299a9e641fe
werogg <supwer00@gmail.com>
2020-03-20 16:34:45 +0100

    Resources - Added new icons Layout - Added Event layout Functional - Added google maps support

--- app/src/main/java/edu/ub/sportshub/event/EventActivity.kt
+++ app/src/main/java/edu/ub/sportshub/event/EventActivity.kt
@@ -2,12 +2,69 @@ package edu.ub.sportshub.event
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import edu.ub.sportshub.R
 
-class EventActivity : AppCompatActivity() {
+class EventActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMapView : MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
+
+        mMapView = findViewById(R.id.mapView)
+
+        mMapView.onCreate(savedInstanceState)
+
+        mMapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val eventName = findViewById<TextView>(R.id.eventNameTextView)
+        val coords = LatLng(41.3855398, 2.1639748)
+        googleMap?.addMarker(MarkerOptions().position(coords).title(eventName.text.toString()))
+        val location = CameraUpdateFactory.newLatLngZoom(coords, 15F)
+        googleMap?.animateCamera(location)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mMapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mMapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mMapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        mMapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mMapView.onLowMemory()
     }
 }
