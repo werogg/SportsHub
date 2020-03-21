3000796e5b7f7ea9274c7a4e22112864a8a9ec6a
werogg <supwer00@gmail.com>
2020-03-21 18:42:30 +0100

    Functional - Added on user click go to profile

--- app/src/main/java/edu/ub/sportshub/home/Users.kt
+++ app/src/main/java/edu/ub/sportshub/home/Users.kt
@@ -1,13 +1,16 @@
 package edu.ub.sportshub.home
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 
 import edu.ub.sportshub.R
+import edu.ub.sportshub.profile.ProfileActivity
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +23,21 @@ class Users : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_users, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userCard = view.findViewById<CardView>(R.id.userCard)
+
+        userCard.setOnClickListener {
+            userCardClicked()
+        }
+    }
+
+    private fun userCardClicked() {
+        val intent = Intent(activity, ProfileActivity::class.java)
+        startActivity(intent)
     }
 
 
