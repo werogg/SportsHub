40f56112f48dff175cd02ee55325ad5e3f108b06
werogg <supwer00@gmail.com>
2020-03-21 17:54:49 +0100

    Functional - Added create event toolbar functionalities - Adedd edit event toolbar functionalities Layout - Removed logout button - Fixed some bugs

--- app/src/main/java/edu/ub/sportshub/event/EditEventActivity.kt
+++ app/src/main/java/edu/ub/sportshub/event/EditEventActivity.kt
@@ -1,13 +1,70 @@
 package edu.ub.sportshub.event
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import de.hdodenhof.circleimageview.CircleImageView
 import edu.ub.sportshub.R
+import edu.ub.sportshub.home.HomeActivity
+import edu.ub.sportshub.profile.ProfileActivity
 
 class EditEventActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_event)
+        setupActivityFunctionalities()
+    }
+
+    private fun setupActivityFunctionalities() {
+        setupListeners()
+    }
+
+    /**
+     * Setup all listeners in the activity
+     */
+    private fun setupListeners() {
+        val textProfile = findViewById<TextView>(R.id.toolbar_secondary_txt_my_profile)
+
+        textProfile.setOnClickListener(){
+            profileClicked()
+        }
+
+        val imageProfile = findViewById<CircleImageView>(R.id.toolbar_secondary_image_my_profile)
+
+        imageProfile.setOnClickListener {
+            profileClicked()
+        }
+
+        val homeText = findViewById<TextView>(R.id.toolbar_secondary_home)
+
+        homeText.setOnClickListener {
+            homeTextClicked()
+        }
+
+        val editEventButton = findViewById<Button>(R.id.edit_event_activity_edit_event_button)
+
+        editEventButton.setOnClickListener {
+            onEditEventButtonClicked()
+        }
+
+    }
+
+    private fun homeTextClicked() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun profileClicked() {
+        val popupIntent = Intent(this, ProfileActivity::class.java)
+        startActivity(popupIntent)
+    }
+
+    private fun onEditEventButtonClicked() {
+        val intent = Intent(this, EventActivity::class.java)
+        startActivity(intent)
     }
 }
