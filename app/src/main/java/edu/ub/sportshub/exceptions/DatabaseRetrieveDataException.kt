e20b3e792aede8a34abb0aba83f3a4f559dc7cc8
werogg <supwer00@gmail.com>
2020-03-22 17:32:37 +0100

    Layout - Hot fixes on dev branch

--- app/src/main/java/edu/ub/sportshub/exceptions/DatabaseRetrieveDataException.kt
+++
@@ -1,6 +0,0 @@
-package edu.ub.sportshub.exceptions
-
-import java.lang.Exception
-
-class DatabaseRetrieveDataException(override val message: String?) : Exception() {
-}
--- app/src/main/java/edu/ub/sportshub/exceptions/RegistrationFailedException.kt
+++
@@ -1,6 +0,0 @@
-package edu.ub.sportshub.exceptions
-
-import java.lang.Exception
-
-class RegistrationFailedException (override val message: String?) : Exception() {
-}
