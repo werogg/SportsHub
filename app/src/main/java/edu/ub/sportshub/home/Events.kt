45ebe1361f2dee6dfc6c910f31c9c3228b595da0
werogg <supwer00@gmail.com>
2020-03-27 19:45:45 +0100

    Events Editing

--- app/src/main/java/edu/ub/sportshub/home/Events.kt
+++ app/src/main/java/edu/ub/sportshub/home/Events.kt
@@ -7,16 +7,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.PopupWindow
 import androidx.cardview.widget.CardView
+import com.google.firebase.auth.FirebaseAuth
 
 import edu.ub.sportshub.R
 import edu.ub.sportshub.event.EventActivity
+import edu.ub.sportshub.helpers.AuthDatabaseHelper
+import edu.ub.sportshub.helpers.StoreDatabaseHelper
+import edu.ub.sportshub.models.User
 
 /**
  * A simple [Fragment] subclass.
  */
 class Events : Fragment() {
+
+    private var storeDatabaseHelper = StoreDatabaseHelper()
+    private var authDatabaseHelper = AuthDatabaseHelper()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,10 +35,18 @@ class Events : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val nikeCard = view.findViewById<CardView>(R.id.nikeCard)
+        showFollowingUsersEvents()
+    }
 
-        nikeCard.setOnClickListener {
-            nikeCardClicked()
+    private fun retrieveUser() {
+
+    }
+
+    private fun showFollowingUsersEvents() {
+        var loggedUserUid = authDatabaseHelper.getCurrentUser()?.uid.toString()
+        storeDatabaseHelper.retrieveUser(loggedUserUid).addOnSuccessListener {
+            var user = it.toObject(User::class.java)
+
         }
 
     }
---
+++ app/src/main/java/edu/ub/sportshub/widgets/EventView.kt
@@ -0,0 +1,8 @@
+package edu.ub.sportshub.widgets
+
+import android.content.Context
+import androidx.cardview.widget.CardView
+
+class EventView(context: Context, eventId: String) : CardView(context) {
+
+}
