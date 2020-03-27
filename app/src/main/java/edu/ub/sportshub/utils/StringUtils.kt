7c7cd6cd1377162411e455e6bc53af00a6afe4c5
werogg <supwer00@gmail.com>
2020-03-27 18:03:48 +0100

    Added util todo to compact strings

---
+++ app/src/main/java/edu/ub/sportshub/utils/StringUtils.kt
@@ -0,0 +1,14 @@
+package edu.ub.sportshub.utils
+
+class StringUtils {
+
+    /**
+     * @param value number of likes
+     * @return string with Xk if > 999 - Xm if > 999k
+     */
+    fun compactNumberString(value : Int) : String {
+        // TODO this function
+        return ""
+    }
+
+}
