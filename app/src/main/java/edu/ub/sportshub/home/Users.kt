cda609e91d0e6d7774ca2b8eeb3315fc92debb91
Carla Morral <carla.morral@gmail.com>
2020-04-08 00:02:59 +0200

    Search users working

--- app/src/main/java/edu/ub/sportshub/home/Users.kt
+++ app/src/main/java/edu/ub/sportshub/home/Users.kt
@@ -3,17 +3,21 @@ package edu.ub.sportshub.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.SearchView
+import android.widget.TextView
 import androidx.cardview.widget.CardView
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.QueryDocumentSnapshot
 
+import com.squareup.picasso.Picasso
 import edu.ub.sportshub.R
 import edu.ub.sportshub.helpers.AuthDatabaseHelper
-import edu.ub.sportshub.helpers.DatabaseHelper
 import edu.ub.sportshub.helpers.StoreDatabaseHelper
 import edu.ub.sportshub.models.User
 import edu.ub.sportshub.profile.ProfileActivity
@@ -38,18 +42,6 @@ class Users : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val userCard = view.findViewById<CardView>(R.id.userCard)
-
-        userCard.setOnClickListener {
-            userCardClicked()
-        }
-
-        val userCard2 = view.findViewById<CardView>(R.id.userCard2)
-
-        userCard2.setOnClickListener {
-            userCard2Clicked()
-        }
-
         val searchView = view.findViewById<SearchView>(R.id.searchView)
 
         searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
@@ -66,18 +58,51 @@ class Users : Fragment() {
     }
 
     private fun searchUsers(query: String) {
+        var n = query.length
+        storeDatabaseHelper.getUsersCollection().whereGreaterThanOrEqualTo("username", query).get()
+            .addOnSuccessListener { users ->
+                for (user in users) {
+                    addUser(user)
+                }
+            }
 
     }
 
-    private fun userCardClicked() {
-        val intent = Intent(activity, ProfileActivity::class.java)
-        startActivity(intent)
+    private fun addUser(user: QueryDocumentSnapshot){
+
+        val userContainer = view?.findViewById<LinearLayout>(R.id.userContainer)
+        userContainer?.removeAllViews()
+
+        val dpSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 130f, context?.resources?.displayMetrics).toInt()
+        val userView = LayoutInflater.from(context).inflate(R.layout.user_view, null);
+        val userViewTitleView = userView.findViewById<TextView>(R.id.username)
+        val userViewFollowView = userView.findViewById<TextView>(R.id.follow)
+        val userViewBannerImage = userView.findViewById<CardView>(R.id.userImage)
+
+        /*
+        Picasso.with(context)
+            .load(user.getData().get("profilePicture").toString())
+            .resize(dpSize, dpSize)
+            .into(userViewBannerImage)
+
+         */
+
+
+        userView.setOnClickListener {
+            val intent = Intent(activity, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        userViewTitleView.text = user.getData().get("username").toString()
+
+        // Deal with following. Get current user and check
+
+        userContainer?.addView(userView)
+
     }
 
-    private fun userCard2Clicked() {
-        val intent = Intent(activity, ProfileOtherActivity::class.java)
-        startActivity(intent)
-    }
 
 
 }
+
+
