28c8c973d3e293a5bf6827da7678c73465686c5a
werogg <supwer00@gmail.com>
2020-05-15 21:53:28 +0200

    blank screen hotfix

--- app/src/main/java/edu/ub/sportshub/MainActivity.kt
+++ app/src/main/java/edu/ub/sportshub/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // If user is not logged re-define intent to login
+
         if (!authDatabaseHelper.isUserLogged()) {
             intent = Intent(this, LoginActivity::class.java)
         } else if (!authDatabaseHelper.getCurrentUser()?.isEmailVerified!!) {
@@ -26,13 +26,12 @@ class MainActivity : AppCompatActivity() {
             intent = Intent(this, LoginActivity::class.java)
         } else {
             // Define intent to RegisterActivity
-            var intent = Intent(this, HomeActivity::class.java)
+            intent = Intent(this, HomeActivity::class.java)
 
             Intent(this, NotificationService::class.java).also { intent ->
                 startService(intent)
             }
-
-            startActivity(intent)
         }
+        startActivity(intent)
     }
 }
